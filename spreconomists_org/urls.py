1451567b69f66835e3ee72748d9c071b20cdb7c0
nerdfiles <nerdfiles@gmail.com>
2016-02-05 22:27:43 -0600

    Template updates.

--- spreconomists_org/urls.py
+++ spreconomists_org/urls.py
@@ -5,8 +5,11 @@ import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
-    url(r'^about$', views.about, name='about'),
     url(r'^events$', views.events, name='events'),
     url(r'^gallery$', views.gallery, name='gallery'),
     url(r'^contact$', views.contact, name='contact'),
 ]
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
+
--- website/models.py
+++ website/models.py
@@ -2,7 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
-from html_field import HTMLField
+
+from html_field.db.models import HTMLField
+from html_field import html_cleaner
+
+c = html_cleaner.HTMLCleaner(allow_tags=['a', 'img', 'em', 'strong'])
+
 
 
 class UserProfile(models.Model):
@@ -27,24 +32,33 @@ class Member(UserProfile):
     Member Extensions to User Profile
     '''
 
-    title = models.CharField(max_length=255)
-    customer_id = models.CharField(max_length=100)
+    create_date = models.DateField(blank=True, null=True)
+    # For storing Stripe Customer ID after purchase is made
+    # @see https://stripe.com/docs/api#customer_object
+    customer_id = models.CharField(max_length=64)
 
 
 class GalleryItem(models.Model):
 
     '''
+    Gallery Items are essentially deferred. Users will be expected to upload
+    their media to a third-party host which might provide for hosting or a
+    hosted embed.
     '''
 
-    title = models.CharField(max_length=255)
-    content = models.HTMLField(max_length=1000)
+    name = models.CharField(max_length=255)
+    pub_date = models.DateField(blank=True, null=True)
+    # Embeddable content from YouTube, etc.
+    content = HTMLField(c, blank=True)
 
 
 class Gallery(models.Model):
 
     '''
+    Galleries are groups of Items with arbitrary HTML chunks. Simpler than django-snippets.
     '''
 
-    title = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    pub_date = models.DateField(blank=True, null=True)
     gallery_item = models.ForeignKey(GalleryItem, on_delete=models.CASCADE)
 
